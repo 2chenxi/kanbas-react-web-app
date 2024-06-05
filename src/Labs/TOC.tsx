8ce459aa0b7b76c50dd1bb44d785c08104de4e73
赵晨曦 <zhaochenxi@zhaochenxideair.mynetworksettings.com>
2024-06-05 18:19:12 -0400

    a2 add my git in toc.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,9 @@
 export default function TOC() {
     return (
       <ul>
+              Please
+<a id="wd-github" href="https://github.com/2chenxi/kanbas-react-web-app.git"> click here</a>
+to get my git repository<br/>
         <li><a id="wd-a"  href="#/Labs">Labs</a></li>
         <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
